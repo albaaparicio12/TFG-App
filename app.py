9677d659b3157acf0165f42179039f2bf9b8142e
Alba <albaaparicio12@gmail.com>
2022-04-24 13:17:28 +0200

    Linked forms in qsvm and qnn

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,17 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
-@app.route('/qsvm/', methods=["POST","GET"])
-def qsvm():
-    return render_template('qsvm.html')
 
-@app.route('/qnn', methods=["POST","GET"])
+@app.route('/qsvm/', methods=["POST", "GET"])
+def qsvm():
+    datos = ['Red', 'Blue', 'Black', 'Orange']
+    return render_template('qsvm.html', datos=datos)
+
+
+@app.route('/qnn/', methods=["POST", "GET"])
 def qnn():
     datos = ['Red', 'Blue', 'Black', 'Orange']
     return render_template('qnn.html', datos=datos)
 
-@app.route('/ejecucion', methods=["POST","GET"])
-def ejecucion():
-    datos = ['Red', 'Blue', 'Black', 'Orange']
-    return render_template('ejecucion.html', datos=datos)
 
+@app.route('/ejecucion', methods=["POST", "GET"])
+def ejecucion():
+    dispositivos = ['Red1', 'Blue2', 'Black3', 'Orange4']
+    if request.method == 'GET':
+        datos = request.args.get('datos')
+        tipoEjecucion = request.args.get('tipoEjecucion')
+    else:
+        tipoEjecucion = "Local"
+        datos = "básico"
+    return render_template('ejecucion.html', datos=datos, tipoEjecucion=tipoEjecucion, dispositivos=dispositivos)
